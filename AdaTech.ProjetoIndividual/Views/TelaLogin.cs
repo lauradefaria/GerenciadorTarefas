a22c0793ef6da0af6ee3e7ca70bfe620f9f5ecb7
lauradefaria <lauradfma@gmail.com>
2024-01-15 17:14:42 -0300

    Add project files.

---
+++ AdaTech.ProjetoIndividual/Views/TelaLogin.cs
@@ -0,0 +1,10 @@
+namespace AdaTech.ProjetoIndividual
+{
+    public partial class TelaLogin : Form
+    {
+        public TelaLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
